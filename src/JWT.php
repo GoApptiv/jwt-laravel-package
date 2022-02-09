c4f5b1188823ce85d0b9ad7c94fb5dceecd99557
manish-kapadi <manish.kapadi@goapptiv.com>
2022-02-09 21:03:56 +0530

    Changed signing algorithm from MD5 to HS256

--- src/JWT.php
+++ src/JWT.php
@@ -6,7 +6,8 @@ use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Collection;
 
-class JWT {
+class JWT
+{
 
     /**
      * Generate token
@@ -16,13 +17,14 @@ class JWT {
      *
      * @return mixed
      */
-    public static function encrypt(Collection $data, Carbon $expiryTime = null) {
+    public static function encrypt(Collection $data, Carbon $expiryTime = null)
+    {
         $header = self::encodeHeader();
         $payload = self::encodePayload(self::generatePayload($data, $expiryTime));
         return implode(".", [
             $header,
             $payload,
-            self::generateSignature($header, $payload),
+            self::base64url_encode(self::generateSignature($header, $payload)),
         ]);
     }
 
@@ -33,13 +35,14 @@ class JWT {
      * @return mixed
      * @throws Exception
      */
-    public static function decrypt($token) {
+    public static function decrypt($token)
+    {
         $parts = explode(".", $token);
         if (count($parts) != 3) {
             throw new Exception("Invalid Token");
         }
 
-        if (self::generateSignature($parts[0], $parts[1]) !== $parts[2]) {
+        if (self::base64url_encode(self::generateSignature($parts[0], $parts[1])) !== $parts[2]) {
             throw new Exception("Invalid Token");
         }
 
@@ -58,8 +61,8 @@ class JWT {
      *
      *
      */
-    private static function generatePayload(Collection $payload, Carbon $expiryTime = null) {
-
+    private static function generatePayload(Collection $payload, Carbon $expiryTime = null)
+    {
         if ($expiryTime !== null) {
             $payload['token_expiry_timestamp'] = $expiryTime->timestamp;
         }
@@ -72,22 +75,31 @@ class JWT {
      *
      * @return string
      */
-    private static function encodeHeader() {
-        return base64_encode(json_encode([
-            "alg" => "MD5",
+    private static function encodeHeader()
+    {
+        return self::base64url_encode(json_encode([
+            "alg" => "HS256",
             "type" => "JWT",
         ]));
     }
 
-    private static function encodePayload($data) {
-        return base64_encode(json_encode($data));
+    private static function encodePayload($data)
+    {
+        return self::base64url_encode(json_encode($data));
     }
 
-    private static function decodePayload($payload) {
+    private static function decodePayload($payload)
+    {
         return json_decode(base64_decode($payload), true);
     }
 
-    private static function generateSignature($header, $payload) {
-        return md5($header . "." . $payload . "." . env("TOKEN_SECRET_KEY"));
+    private static function generateSignature($header, $payload)
+    {
+        return hash_hmac('sha256', "$header.$payload", env("TOKEN_SECRET_KEY"), true);
+    }
+
+    private static function base64url_encode($data)
+    {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
 }
